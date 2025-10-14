e490e32f972f5b386176364dce08c03b2936a155
Walker Lockard <walker.b.lockard@gmail.com>
2025-10-14 09:09:20 -0500

    correct nextjs example

--- examples/nextjs-example/src/app/(app)/layout.tsx
+++ examples/nextjs-example/src/app/(app)/layout.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { OPENROUTER_KEY_LOCALSTORAGE_KEY } from "@/lib/config";
-import { useApiKey } from "@/lib/hooks/use-api-key";
 import { OpenRouterClientProvider } from "@/lib/hooks/use-openrouter-client";
 
 export default function Layout({
