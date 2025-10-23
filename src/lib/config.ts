42c377ceb11711573de442f7ec5ca5986ff23837
Matt Apperson <me@mattapperson.com>
2025-10-23 16:13:29 -0400

    version bump

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -59,8 +59,8 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0.0",
-  sdkVersion: "0.0.1-beta.7",
+  sdkVersion: "0.0.1-beta.8",
   genVersion: "2.730.0",
   userAgent:
-    "speakeasy-sdk/typescript 0.0.1-beta.7 2.730.0 1.0.0 @openrouter/sdk",
+    "speakeasy-sdk/typescript 0.0.1-beta.8 2.730.0 1.0.0 @openrouter/sdk",
 } as const;
